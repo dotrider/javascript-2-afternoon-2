b193d35a263c22b273583f6f99cc1bf7344d9e31
dotrider <56666461+dotrider@users.noreply.github.com>
2020-01-09 23:55:19 -0700

    arrays

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr){
+  return arr[0];
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+  return arr[arr.length-1];
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,8 +52,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
-
+function looper(family) {
+  for ( i = 0; i < family.length; i++) {
+    alert(family[i]);
+  }
+};
 
 ////////// PROBLEM 4 //////////
 
@@ -64,8 +71,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
-
-
+function reversedLooper (letters){
+  for ( i = letters.length-1; i >= 0; i-- ){
+  alert(letters[i]);
+}
+};
 ////////// PROBLEM 5 //////////
 
 // Do not edit the code below.
@@ -79,8 +89,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
-
-
+function evenFinder(nums){
+  let newArr = [];
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      newArr.push(nums[i]);
+    } 
+  }
+  return newArr;
+}
 
 
 
@@ -108,7 +125,19 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 
 //Code Here
 
+function divider (numbersArray){
+  let evenArrays = [];
+  let oddArrays = [];
 
+    for (i = 0; i < numbersArray.length; i++){
+      if (numbersArray[i] % 2 === 0 ){
+        evenArrays.push(numbersArray[i]);
+      } else {
+        oddArrays.push(numbersArray[i]);
+      }
+  } return [evenArrays,oddArrays];
+
+}
 
 ////////// PROBLEM 7 //////////
 
